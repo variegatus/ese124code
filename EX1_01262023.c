282c993c3d6018e5d297c0586330d4d7827e168e
variegatus <100755754+variegatus@users.noreply.github.com>
2023-03-21 13:38:23 -0400

    Add files via upload

---
+++ EX1_01262023.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main () {
+
+   int a, b, c, d, sum, val1, val2;
+   float w, v;
+   int aa, bb;
+   
+   printf (" a = ");
+   scanf ("%d", &a); 
+   
+   printf (" b = ");
+   scanf ("%d", &b); 
+   
+   printf ("a = %d b = %d\n", a, b);
+   
+   sum = a + b;
+   
+   printf ("sum = %d\n", sum);
+   
+   c = a - b;
+   
+   printf ("dif = %d\n", c);
+   
+   d = a * b;
+   printf ("prod = %d\n", d);
+   
+   
+}
